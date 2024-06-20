ce14c13f54a376e53b417a641d7b3339a62c142d
amr <amr442028@gmail.com>
2024-06-20 03:56:08 +0300

    update env.local

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -14,7 +14,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
       <CartProvider>
         <html lang="en">
           <head>
