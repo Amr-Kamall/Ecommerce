c064bb5a0a468bd71dd8782e2b3684c787afd718
amr <amr442028@gmail.com>
2024-06-21 19:54:01 +0300

    check page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,13 +2,15 @@ import Slider from "./components/Slider";
 import FeaturedProducts from "./components/FeaturedProducts";
 import Categories from "./components/categories";
 import NewList from "./components/NewProducts";
+import Contact from "./contact/page";
 function HomePage() {
   return (
     <>
-      <Slider />
-      <FeaturedProducts />
-      <Categories />
-      <NewList />
+      {/* <Slider /> */}
+      {/* <FeaturedProducts /> */}
+      {/* <Categories /> */}
+      {/* <NewList /> */}
+      <Contact />
     </>
   );
 }
