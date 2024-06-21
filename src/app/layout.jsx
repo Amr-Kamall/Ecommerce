508252ce1523599224ee6ecf221362708c24d0ee
amr <amr442028@gmail.com>
2024-06-21 07:14:15 +0300

    update key

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -7,21 +7,8 @@ import CartProvider from "./context/CartContext";
 import { Suspense } from "react";
 import Loading from "./loading";
 export default function RootLayout({ children }) {
-  console.log(
-    "NEXT_PUBLIC_CLERK_FRONTEND_API_KEY:",
-    process.env.NEXT_PUBLIC_CLERK_FRONTEND_API_KEY
-  );
-  console.log("amr yalaaaaaaa");
-  const clerkFrontendApi = process.env.NEXT_PUBLIC_CLERK_FRONTEND_API_KEY;
-  if (!clerkFrontendApi) {
-    throw new Error(
-      "Missing NEXT_PUBLIC_CLERK_FRONTEND_API_KEY environment variable"
-    );
-  }
   return (
-    <ClerkProvider
-      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
-    >
+    <ClerkProvider>
       <CartProvider>
         <html lang="en">
           <head>
