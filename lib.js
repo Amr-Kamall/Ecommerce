772590378ae07f3417a4493e88cb71545852dcfb
amr <amr442028@gmail.com>
2024-06-23 16:00:29 +0300

    update api

--- lib.js
+++ lib.js
@@ -11,36 +11,45 @@ export async function getFeaturedProduct() {
 // }
 
 export async function getCategories() {
-  const api = "http://localhost:1337/api/categories?fields=name&populate=image";
+  // const api = "http://localhost:1337/api/categories?fields=name&populate=image";
+  const api = `http://localhost:1337/api/products?populate=image`;
+
   const res = await fetch(api);
   const data = await res.json();
   return data.data;
 }
 
 export async function getAllProducts() {
-  const api =
-    "http://localhost:1337/api/categories/1?fields=name&populate[products][populate]=image";
+  // const api =
+  //   "http://localhost:1337/api/categories/1?fields=name&populate[products][populate]=image";
+  const api = `http://localhost:1337/api/products?populate=image`;
+
   const res = await fetch(api);
   const data = await res.json();
   return data.data.attributes.products.data;
 }
 export async function getNewsProduct() {
-  const api =
-    "http://localhost:1337/api/categories/6?fields=name&populate[products][populate]=image";
+  // const api ="http://localhost:1337/api/categories/6?fields=name&populate[products][populate]=image";
+  const api = `http://localhost:1337/api/products?populate=image`;
+
   const res = await fetch(api);
   const data = await res.json();
   return data.data;
 }
 
 export async function getCategoryProduct(id) {
-  const api = `http://localhost:1337/api/categories/${id}?fields=name&populate[products][populate]=image`;
+  // const api = `http://localhost:1337/api/categories/${id}?fields=name&populate[products][populate]=image`;
+  const api = `http://localhost:1337/api/products?populate=image`;
+
   const res = await fetch(api);
   const data = await res.json();
   return data.data;
 }
 
 export async function getDeals() {
-  const api = "http://localhost:1337/api/deals?populate=image";
+  // const api = "http://localhost:1337/api/deals?populate=image";
+  const api = `http://localhost:1337/api/products?populate=image`;
+
   const res = await fetch(api);
   const data = await res.json();
   return data.data;
