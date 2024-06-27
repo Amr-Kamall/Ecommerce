a9af36225586ba8b3844ab985d8e14d54c6a9f7e
amr <amr442028@gmail.com>
2024-06-27 18:33:57 +0300

    update footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -9,11 +9,8 @@ function Footer() {
 
   return (
     <div className="bg-gray-100">
-      {/* wrapper */}
       <div className="py-6 w-4/5 mx-auto text-sm">
-        {/* TOP */}
         <div className="flex flex-col md:flex-row justify-between gap-24">
-          {/* LEFT */}
           <div className="w-full md:w-1/2 lg:w-1/4 flex flex-col gap-8">
             <Link href="/">
               <div className="text-2xl tracking-wide">AMRO</div>
@@ -25,14 +22,28 @@ function Footer() {
             <span className="font-semibold">hello@amro.dev</span>
             <span className="font-semibold">+1 234 567 890</span>
             <div className="flex gap-6">
-              <Image src="/facebook.png" alt="" width={16} height={16} />
-              <Image src="/instagram.png" alt="" width={16} height={16} />
-              <Image src="/youtube.png" alt="" width={16} height={16} />
-              <Image src="/pinterest.png" alt="" width={16} height={16} />
-              <Image src="/x.png" alt="" width={16} height={16} />
+              <Image
+                src="/facebook.png"
+                alt="Facebook"
+                width={16}
+                height={16}
+              />
+              <Image
+                src="/instagram.png"
+                alt="Instagram"
+                width={16}
+                height={16}
+              />
+              <Image src="/youtube.png" alt="YouTube" width={16} height={16} />
+              <Image
+                src="/pinterest.png"
+                alt="Pinterest"
+                width={16}
+                height={16}
+              />
+              <Image src="/x.png" alt="X" width={16} height={16} />
             </div>
           </div>
-          {/* CENTER */}
           <div className="hidden lg:flex justify-between w-1/2">
             <div className="flex flex-col justify-between">
               <h1 className="font-medium text-lg">COMPANY</h1>
@@ -65,7 +76,6 @@ function Footer() {
               </div>
             </div>
           </div>
-          {/* RIGHT */}
           <div className="w-full md:w-1/2 lg:w-1/4 flex flex-col gap-8">
             <h1 className="font-medium text-lg">SUBSCRIBE</h1>
             <p>
@@ -82,23 +92,32 @@ function Footer() {
             </div>
             <span className="font-semibold">Secure Payments</span>
             <div className="flex justify-between">
-              <Image src="/discover.png" alt="" width={40} height={20} />
-              <Image src="/skrill.png" alt="" width={40} height={20} />
-              <Image src="/paypal.png" alt="" width={40} height={20} />
-              <Image src="/mastercard.png" alt="" width={40} height={20} />
-              <Image src="/visa.png" alt="" width={40} height={20} />
+              <Image
+                src="/discover.png"
+                alt="Discover"
+                width={40}
+                height={20}
+              />
+              <Image src="/skrill.png" alt="Skrill" width={40} height={20} />
+              <Image src="/paypal.png" alt="PayPal" width={40} height={20} />
+              <Image
+                src="/mastercard.png"
+                alt="MasterCard"
+                width={40}
+                height={20}
+              />
+              <Image src="/visa.png" alt="Visa" width={40} height={20} />
             </div>
           </div>
         </div>
-        {/* BOTTOM */}
         <div className="flex flex-col md:flex-row items-center justify-between gap-8 mt-16">
-          <div className="">© 2024 Lama Shop</div>
+          <div>© 2024 Lama Shop</div>
           <div className="flex flex-col gap-8 md:flex-row">
-            <div className="">
+            <div>
               <span className="text-gray-500 mr-4">Language</span>
               <span className="font-medium">United States | English</span>
             </div>
-            <div className="">
+            <div>
               <span className="text-gray-500 mr-4">Currency</span>
               <span className="font-medium">$ USD</span>
             </div>
